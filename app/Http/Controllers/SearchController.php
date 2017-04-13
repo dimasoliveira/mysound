45b17ffae5bf716f012412cd74e00f57d8187aae
Dimas Oliveira <d.oliveira@live.nl>
2017-04-13 13:28:37 +0200

    Search function and view, searches audio and users at same time

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Audio;
+use App\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function index(){
       return view('search');
+    }
+
+    public function searchRequest(Request $request){
+
+    $audio_results = Audio::where('title','LIKE','%'.$request->search.'%')->orWhere('artist','LIKE','%'.$request->search.'%')->where('published',1)->get();
+    $user_results =  User::where('username','LIKE','%'.$request->search.'%')->orWhere('firstname','LIKE','%'.$request->search.'%')->orWhere('lastname','LIKE','%'.$request->search.'%')->get();
+
+    //dd(!$audio_results->isEmpty());
+
+    if ($audio_results->isEmpty()){
+      $audio_results = NULL;
+    }
+
+    if ($user_results->isEmpty()){
+        $user_results = NULL;
+      }
+
+    return view('search',compact('audio_results','user_results'));
     }
 }
---
+++ resources/views/search.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('expanded-navbar')
+    <div class="flex-container nav-content .center-align">
+
+        <ul class="tabs tabs-transparent row">
+            <li class="flex-item valign tab"><a class="active" href="#test1">Search</a></li>
+            <li class="flex-item valign tab"><a target="_self" href="{{ route('timeline.show') }}">Timeline</a></li>
+            <li class="flex-item valign tab"><a href="{{ route('profile.show', Auth::user()->slug) }}">Profile</a></li>
+        </ul>
+
+    </div>
+@endsection
+
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 m12">
+            <div class="card blue-grey darken-1">
+                <nav>
+                    <div class="nav-wrapper">
+
+                        {!! Form::open(['route'=>'search.request']) !!}
+
+                        {{ csrf_field() }}
+
+                        <div class="input-field light-blue">
+                                <input name="search" id="search" type="search" required>
+                                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                                <i class="material-icons">close</i>
+                            </div>
+                        {{ Form::close() }}
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    @if(isset($audio_results))
+    <h6>Audio Results</h6>
+    <hr>
+           <div class="row">
+                    @foreach($audio_results as $result)
+
+                        <div class="col s2 m2">
+                            <div class="card hoverable">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                    <p class="z-depth-2 card-title activator" style="font-size: 15px!important; color: white;background-color: rgba(0,0,0,0.18);width: 100%;padding-top: 0;padding-bottom: 0">{{ $result->title }}<br>{{ $result->artist }} </p>
+                                    <span id="play-{{ $result->id }}" style="right: 0!important; bottom:0; margin: 10px; padding: 0;" class="card-title btn-floating btn-large waves-effect waves-light blue right"><i class="large material-icons">play_arrow</i></span>
+                                    <img class="activator" src="{{ $result->coverart }}" style="height: auto;width: 100%">
+                                </div>
+                                <div class="card-content" style="padding: 0;line-height: 0;">
+                                </div>
+                                <div class="card-reveal">
+                                    <p class="grey-text text-darken-4">{{ $result->title }}<i class="material-icons right">close</i></p>
+                                    <p>Artist: <br>{{ $result->artist }}</p>
+                                    <p>Album: <br><a href="{{ route("myaudio.album.show",$result->album->slug) }}">{{ $result->album->name }}</a></p>
+                                    <p>Year: <br>{{ $result->year }}</p>
+                                </div>
+                                <div class="card-action">
+                                    <h6 class="header">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($result->created_at))->diffForHumans() .' by '. $result->user->username }}
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+    @endif
+
+
+    @if(isset($user_results))
+    <h6>User Results</h6>
+    <hr>
+
+    @foreach($user_results as $result)
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="card blue-grey darken-1"> {{ $result->username }}
+                </div>
+            </div>
+        </div>
+    @endforeach
+        @endif
+</div>
+
+@if(isset($audio_results))
+<script>
+
+  $(function () {
+
+    @foreach($audio_results as $result)
+
+      $("#play-{{ $result->id }}").click(function(){
+      $(".sm2-playlist-bd").html('<ul class="sm2-playlist-bd"><li><a href="{{ $result->filename }}"><b>{{ $result->artist }}</b> - {{ $result->title }} @if($result->explicit)<span class="label">Explicit</span>@endif</a></li></ul>');
+      window.sm2BarPlayers[0].playlistController.playItemByOffset();
+    });
+
+      @endforeach
+
+  });
+</script>
+    @endif
+@endsection
