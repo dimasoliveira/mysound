8751134ea2b581049c3980112ac180bab45940c2
Dimas Oliveira <d.oliveira@live.nl>
2017-04-26 10:53:23 +0200

    HomeController renamed to TimelineController

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -3,15 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Audio;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 
 class TimelineController extends Controller
 {
-  public function index(){
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
 
+        foreach (Auth::user()->followings as $following){
+          $followinglist[] = $following->id;
+        }
 
-    return view('home');
-  }
+        $followinglist[] = Auth::user()->id;
+
+      // dd($followings = Auth::user()->followings);
+        $posts = Audio::whereIn('user_id',$followinglist)->where('published', 1)->orderBy('created_at','desc')->get();
+        //Audio::orderBy('created_at','asc')->whereIn('user_id',$followinglist)->get();
+        //dd(Audio::all()->whereIn('user_id',$followinglist)->where('published', 1));
+
+        return view('timeline',compact('posts'));
+    }
 }
